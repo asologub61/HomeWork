7d0b06636e1ffb9447ab417bb70c005797620648
asologub61 <andrey-s-workmail@yandex.ru>
2022-08-11 01:43:46 +0300

    Homework for Otus lesson8 , fix TestSystem2

--- src/TestSystem2/LogicTest.java
+++ src/TestSystem2/LogicTest.java
@@ -7,9 +7,30 @@ import java.util.Scanner;
 public class LogicTest {
     CountForTest countAnswers = new CountForTest();
     Scanner scan = new Scanner(System.in);
+    QuestionsAndAnswersVar questionOne = new QuestionsAndAnswersVar("Сколько простых типов в Java?",
+                                                                    "1) 4  \n2) 6  \n3) 8  \n4) 10");
+    QuestionsAndAnswersVar questionTwo = new QuestionsAndAnswersVar("Какая команда компилирует файл в Java?",
+                                                                    "1) javac  \n2) java  \n3) class  \n4) enum");
+    QuestionsAndAnswersVar questionThree = new QuestionsAndAnswersVar("Какой цикл используется когда не известно количество итераций?",
+                                                                      "1) for  \n2) for each  \n3) while  \n4) do while");
 
     public void chooseYourQuestion(byte userChoose) {
-        QuestionsAndAnswersVar userChooseQuestion = new QuestionsAndAnswersVar(userChoose);
+//        QuestionsAndAnswersVar userChooseQuestion = new QuestionsAndAnswersVar(userChoose);
+        switch (userChoose){
+            case(1):
+                questionOne.screenInput();
+                break;
+            case(2):
+                questionTwo.screenInput();
+                break;
+            case(3):
+                questionThree.screenInput();
+                break;
+            default:
+                System.out.println("Неизвестная операция");
+        }
+        System.out.println();
+
         System.out.println("Выберите вариант ответа:");
         Byte userAnswer = scan.nextByte();
         if((userChoose == 1 && userAnswer != 3)
--- src/TestSystem2/QuestionsAndAnswersVar.java
+++ src/TestSystem2/QuestionsAndAnswersVar.java
@@ -2,30 +2,45 @@ package TestSystem2;
 
 public class QuestionsAndAnswersVar {
 
-    private String questionOne = "Сколько простых типов в Java?";
-    private String questionTwo = "Какая команда компилирует файл в Java?";
-    private String questionThree = "Какой цикл используется когда не известно количество итераций?";
-    private  String varForQuestionOne = "1) 4  \n2) 6  \n3) 8  \n4) 10";
-    private String varForQuestionTwo = "1) javac  \n2) java  \n3) class  \n4) enum";
-    private String varForQuestionThree = "1) for  \n2) for each  \n3) while  \n4) do while";
+    private String question;
+    private String answer;
 
+    public QuestionsAndAnswersVar(String inputQuestion, String inputAnswer) {
+        question = inputQuestion;
+        answer = inputAnswer;
 
-    public QuestionsAndAnswersVar(byte numOfQuestion) {
-        switch (numOfQuestion) {
-            case 1:
-                System.out.println(questionOne + "\n" + varForQuestionOne);
-                break;
-            case 2:
-                System.out.println(questionTwo + "\n" + varForQuestionTwo);
-                break;
-            case 3:
-                System.out.println(questionThree + "\n" + varForQuestionThree);
-                break;
-            default:
-                System.out.print("Неизвестная операция");
-                break;
-        }
     }
-}
+
+    public void screenInput(){
+        System.out.println(question);
+        System.out.println(answer);
+
+    }
+
+//    private String questionOne= "Сколько простых типов в Java?";
+//    private String questionTwo = "Какая команда компилирует файл в Java?";
+//    private String questionThree = "Какой цикл используется когда не известно количество итераций?";
+//    private  String varForQuestionOne = "1) 4  \n2) 6  \n3) 8  \n4) 10";
+//    private String varForQuestionTwo = "1) javac  \n2) java  \n3) class  \n4) enum";
+//    private String varForQuestionThree = "1) for  \n2) for each  \n3) while  \n4) do while";
+
+
+//    public QuestionsAndAnswersVar(byte numOfQuestion) {
+//        switch (numOfQuestion) {
+//            case 1:
+//                System.out.println(questionOne + "\n" + varForQuestionOne);
+//                break;
+//            case 2:
+//                System.out.println(questionTwo + "\n" + varForQuestionTwo);
+//                break;
+//            case 3:
+//                System.out.println(questionThree + "\n" + varForQuestionThree);
+//                break;
+//            default:
+//                System.out.print("Неизвестная операция");
+//                break;
+//        }
+    }
+
 
 
